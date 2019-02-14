42efec1fe075ee20dbe6d9965c4019826b0e7b7e
Cristoba <lobo12981298@gmail.com>
2019-02-14 10:45:17 -0500

    Proyecto Iniciado

---
+++ src/sisjara/Sisjara.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisjara;
+
+import java.awt.*;
+import javax.swing.*;
+
+import sisjara.Movimiento_01.Compra.frmcoCotizacion;
+import sisjara.Movimiento_01.Compra.frmcoCotizacionE;
+import sisjara.Movimiento_01.Compra.frmcoCotizacionEP;
+import sisjara.Movimiento_01.Compra.frmcoDocumento;
+import sisjara.Movimiento_01.Compra.frmcoDocumentoE;
+import sisjara.Movimiento_01.Compra.frmcoDocumentoEP;
+import sisjara.Movimiento_01.Compra.frmcoDocumentoI;
+import sisjara.Movimiento_01.Compra.frmcoDocumentoIS;
+import sisjara.Movimiento_01.Compra.frmcoDocumentoN;
+import sisjara.Movimiento_01.Compra.frmcoDocumentoNE;
+import sisjara.Movimiento_01.Compra.frmcoDocumentoNEP;
+import sisjara.Movimiento_01.Compra.frmcoDocumentoR;
+import sisjara.Movimiento_01.Compra.frmcoOrden;
+import sisjara.Movimiento_01.Compra.frmcoOrdenE;
+import sisjara.Movimiento_01.Compra.frmcoOrdenEP;
+import sisjara.Movimiento_01.Compra.frmcoOrdenS;
+import sisjara.Movimiento_01.Compra.frmcoRequerimiento;
+import sisjara.Movimiento_01.Compra.frmcoRequerimientoE;
+import sisjara.Movimiento_01.Compra.frmcoRequerimientoEP;
+import sisjara.Movimiento_01.Compra.frmcoSolicitud;
+import sisjara.Movimiento_01.Compra.frmcoSolicitudE;
+import sisjara.Movimiento_01.Reclamo.frmReclamo;
+import sisjara.Movimiento_01.Reclamo.frmReclamoE;
+import sisjara.Movimiento_01.Reclamo.frmrecAsunto;
+import sisjara.Movimiento_01.Reclamo.frmrecAsuntoE;
+import sisjara.Movimiento_01.Seguimiento.frmveAccion;
+import sisjara.Movimiento_01.Seguimiento.frmveAccionE;
+import sisjara.Movimiento_01.Seguimiento.frmveAsunto;
+import sisjara.Movimiento_01.Seguimiento.frmveAsuntoE;
+import sisjara.Movimiento_01.Seguimiento.frmveControlES;
+import sisjara.Movimiento_01.Seguimiento.frmveControlESE;
+import sisjara.Movimiento_01.Seguimiento.frmveDocumentoES;
+import sisjara.Movimiento_01.Seguimiento.frmveGuiaES;
+import sisjara.Movimiento_01.Seguimiento.frmveRespuesta;
+import sisjara.Movimiento_01.Seguimiento.frmveRespuestaE;
+import sisjara.Movimiento_01.Venta.frmveControlEP;
+import sisjara.Movimiento_01.Venta.frmveDocumento;
+/**
+ *
+ * @author Jara Resistencias
+ */
+public class Sisjara {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*new frmcoCotizacion().setVisible(true);*/
+         new frmveDocumento().setVisible(true);
+    }
+    
+}
